cc1eac592b37043c210900d51f415e1350f0bc76
unlomtrois <vadim.kuz02@gmail.com>
2025-06-25 23:04:33 +0300

    feat: add initial configuration for Zig testing

--- build.zig
+++ build.zig
@@ -19,12 +19,15 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
+    const test_step = b.step("test", "Run unit tests");
+    const test_filter = b.option([]const []const u8, "test-filter", "Filter for test");
+
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
+        .filters = test_filter orelse &.{},
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 }
